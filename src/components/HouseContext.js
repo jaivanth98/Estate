047025dc84aab8e213c7765669459ddf070103c4
jai-sataiva <jai@sataiva.com>
2023-04-26 18:20:27 +0530

    second change

--- src/components/HouseContext.js
+++ src/components/HouseContext.js
@@ -1,7 +1,57 @@
-import React from 'react';
+import React,{useState,useEffect,createContext} from 'react';
+import {housesData} from "../data"
 
-const HouseContext = () => {
-  return <div>HouseContext</div>;
+export const HouseContext =createContext()
+const HouseContextProvider = ({children}) => {
+  const [houses,setHouses]=useState(housesData)
+  const [country,setCountry]=useState("Location (any)")
+  const [countries, setCountries]=useState([])
+  const [property, setProperty]=useState("Property type (any)")
+  const [properties, setProperties]=useState([])
+  const [price, setPrice]=useState("Price range (any)")
+  const [loading,setLoading]=useState(false)
+
+  useEffect(()=>{
+    const allCountries =houses.map((house)=>{
+      return house.country
+    })
+    const uniqueCountries = ["Location (any)",... new Set(allCountries)]
+    setCountries(uniqueCountries)
+  },[])
+
+
+  useEffect(()=>{
+    const allProperties =houses.map((house)=>{
+      return house.type
+    })
+    console.log(allProperties)
+    const uniqueProperties = ["Location (any)",... new Set(allProperties)]
+    setProperties(uniqueProperties)
+  },[])
+
+  const handleClick =()=>{
+     const isDefault = (str) =>{
+      return str.split(" ").includes("(any)")
+     }
+     
+  }
+
+
+  return <HouseContext.Provider value={{
+    houses,
+    setCountry,
+    countries,
+    property,
+    setProperty,
+    properties,
+    price,
+    loading,
+    country,
+    setPrice,
+    handleClick
+  }}>
+    {children}
+    </HouseContext.Provider>;
 };
 
-export default HouseContext;
+export default HouseContextProvider;
